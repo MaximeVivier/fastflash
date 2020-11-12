f25ccc974930dfd689ed7007e87c1c2b834d7b5d
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-13 00:08:46 +0100

    changes made so that all api tests pass

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports =  process.env.NODE_ENV === 'production' ? ({
-    server: {
+    api: {
       port: process.env.PORT_API
     },
     db: {
       url: process.env.MONGODB_URL
     }
   }) : ({
-    server: {
+    api: {
       port: process.env.PORT_API_DEV
     },
     db: {
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { app, mongoose } = require('./api/app');
-const { server, db } = require('./config');
+const { api, db } = require('./config');
 
 mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
-  app.listen(server.port, () => {
+  app.listen(api.port, () => {
     console.log(`NODE_ENV --> ${process.env.NODE_ENV}`);
-    console.log(`Now listening for requests on port ${server.port}`);
+    console.log(`Now listening for requests on port ${api.port}`);
   });
 });
