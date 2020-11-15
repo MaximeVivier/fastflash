fa4962211ac46ff0f8c7722540592cce62b3ace9
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-15 21:15:24 +0100

    log variables to understand

--- tests/api/api-test.js
+++ tests/api/api-test.js
@@ -5,6 +5,10 @@ const { api, db } = require('../../config');
 let server;
 
 const start = async () => new Promise((res) => {
+  console.log('process.env', process.env);
+  console.log('\n\nNODE_ENV --> ', NODE_ENV);
+  console.log('\n\napi --> ', api);
+  console.log('\n\ndb --> ', db);
   mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
   mongoose.connection.once('open', () => {
     console.log('Connected to database');
