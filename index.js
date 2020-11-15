3b6abb20ec588bef8fb59e1ab537b02cfdc68e3b
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-15 21:18:13 +0100

    test ci

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { app, mongoose } = require('./api/app');
 const { api, db } = require('./config');
 
 // Connect to db
+console.log('process.env', process.env);
+console.log('\n\nNODE_ENV --> ', NODE_ENV);
+console.log('\n\napi --> ', api);
+console.log('\n\ndb --> ', db);
 mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
