f2d9eb840d9d6c2d80766bf077b32f3236a7e6b8
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-15 22:39:03 +0100

    remove comments and pipeline ci

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const { app, mongoose } = require('./api/app');
 const { api, db } = require('./config');
 
 // Connect to db
-console.log('process.env', process.env);
-console.log('\n\napi --> ', api);
-console.log('\n\ndb --> ', db);
 mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
   // Once the connection to the db is done, launch the server
   app.listen(api.port, () => {
-    console.log(`NODE_ENV --> ${process.env.NODE_ENV}`);
     console.log(`Now listening for requests on port ${api.port}`);
   });
 });
