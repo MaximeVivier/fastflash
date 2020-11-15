f7fdc01f27c3413cd7289d2592d562cf32434e39
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-15 22:33:20 +0100

    test ci

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports =  process.env.NODE_ENV === 'production' ? ({
     api: {
-      port: process.env.PORT_API
+      port: process.env.PORT_API | 4000
     },
     db: {
       url: process.env.MONGODB_URL
     }
   }) : ({
     api: {
-      port: process.env.PORT_API_DEV
+      port: process.env.PORT_API_DEV | 4000
     },
     db: {
       url: process.env.MONGODB_URL_DEV
