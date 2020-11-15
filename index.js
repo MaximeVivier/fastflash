289edc78b554bfc4589a82b434c5cad3cd3928c2
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-15 20:57:43 +0100

    add comments to index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { app, mongoose } = require('./api/app');
 const { api, db } = require('./config');
 
+// Connect to db
 mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
+  // Once the connection to the db is done, launch the server
   app.listen(api.port, () => {
     console.log(`NODE_ENV --> ${process.env.NODE_ENV}`);
     console.log(`Now listening for requests on port ${api.port}`);
