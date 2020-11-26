079dd26d156104840cb654f135cbf1850d6e7cd0
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-26 12:44:50 +0100

    remove 4000 default

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports =  process.env.NODE_ENV === 'production' ? ({
     api: {
-      port: process.env.PORT_API | 4000
+      port: process.env.PORT_API
     },
     db: {
       url: process.env.MONGODB_URL
     }
   }) : ({
     api: {
-      port: process.env.PORT_API_DEV | 4000
+      port: process.env.PORT_API_DEV
     },
     db: {
       url: process.env.MONGODB_URL_DEV
