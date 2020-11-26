addb4217dc8f279502726f6b207076b774b40440
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-26 14:42:06 +0100

    remove useless logs

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const { app, mongoose } = require('./api/app');
 const { api, db } = require('./config');
 
 // Connect to db
-console.log('db --> ', db);
-console.log('typeof db.url --> ', typeof db.url);
-console.log('api --> ', api);
-console.log('typeof api.port --> ', typeof api.port);
-console.log('api.port == 4000 --> ', api.port == 4000);
 mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
