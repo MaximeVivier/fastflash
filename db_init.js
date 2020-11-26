02d35bd7a145322899ddee622fc35b032e864b0e
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-26 14:41:51 +0100

    make promise out of the db_init script

--- db_init.js
+++ db_init.js
@@ -4,13 +4,17 @@ const db_data = require('./db_data.json');
 const { exists } = require('./api/models/card');
 const { db } = require('./config');
 
-mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
-mongoose.connection.once('open', async () => {
-  console.log('connected to database');
-  console.log('Initializing the db by erasing everything then populate it');
-  await Card.remove({});
-  await Card.insertMany(db_data);
-  console.log('data inserted');
-  process.exit();
-
+const db_init = () => new Promise( async (res) => {
+  mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
+  mongoose.connection.once('open', async () => {
+    console.log('connected to database');
+    console.log('Initializing the db by erasing everything then populate it');
+    await Card.remove({});
+    await Card.insertMany(db_data);
+    console.log('data inserted');
+    mongoose.connection.close();
+    res();
+  });
 });
+
+module.exports = db_init;
