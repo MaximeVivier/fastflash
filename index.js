e2b191601de65a82b365db0a3bb028a9e69f8d18
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-26 12:42:28 +0100

    test ci 3

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { api, db } = require('./config');
 
 // Connect to db
 console.log('db --> ', db);
+console.log('typeof db.url --> ', typeof db.url);
 console.log('api --> ', api);
+console.log('typeof api.port --> ', typeof api.port);
+console.log('api.port == 4000 --> ', api.port == 4000);
 mongoose.connect('',  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
