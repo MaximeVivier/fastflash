a9c952bb1526a14dad0c54278ce2b3e53762f125
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-26 12:52:54 +0100

    test ci 4

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log('typeof db.url --> ', typeof db.url);
 console.log('api --> ', api);
 console.log('typeof api.port --> ', typeof api.port);
 console.log('api.port == 4000 --> ', api.port == 4000);
-mongoose.connect('',  {useNewUrlParser: true}, { useUnifiedTopology: true } )
+mongoose.connect(db.url,  {useNewUrlParser: true}, { useUnifiedTopology: true } )
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
   // Once the connection to the db is done, launch the server
