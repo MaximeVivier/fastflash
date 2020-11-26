de99512f73855bbda3aaa04523b7f9f7c8e0d1c5
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2020-11-26 11:27:37 +0100

    create library vue

---
+++ client/src/components/Libraries/LibraryItemList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-on:click="goToThatLibrary">
+    {{ library.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  methods: {
+    goToThatLibrary() {
+      this.$router.push({ name: `library`, params: { libName: this.library.name } })
+    }
+  },
+  props: {
+    library: Object,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Libraries/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Choose the library you want to study</h1>
+    <div>
+      <LibraryItemList v-for="library in libraries" :key="library.name" v-bind:library="library" />
+    </div>
+  </div>
+</template>
+
+<script>
+import LibraryItemList from './LibraryItemList';
+import gql from 'graphql-tag';
+
+export default {
+  name: 'DisplayCard',
+  components: {
+    LibraryItemList,
+  },
+  methods: {},
+  apollo: {
+    libraries: {
+      query: gql`{
+        libraries {
+          name
+        }
+      }`,
+      update: data => data.libraries
+    },
+  },
+  data() {
+    return {
+      libraries: []
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
